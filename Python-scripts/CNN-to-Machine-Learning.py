842d1256b98fb976c8874e0675cab145b57c922d
Aythami Estévez Olivas <aythae@gmail.com>
2017-06-14 20:18:40 +0200

    Acabada extracción de características

--- Python-scripts/CNN-to-Machine-Learning.py
+++ Python-scripts/CNN-to-Machine-Learning.py
@@ -1,6 +1,9 @@
 # https://github.com/fchollet/keras/issues/431#issuecomment-213702147
 
 import numpy as np
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+from datetime import datetime
+
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.svm import SVC
@@ -8,53 +11,50 @@ from sklearn.svm import SVC
 from extract_features import create_feature_extractor
 
 imgSize = 64
-useAditional = True
-keepAspectRatio = True
-loadFeatures = True
+loadFeatures = False
 RDM = 17
-percentTrainForValidation = 0.15
 
 clasif="RF" # RF/SVM
 SEPARATOR = "=============================================================" + \
             "==================="
 
 
-def svc(traindata, trainlabel, validData, validLabel):#, testdata):
+def svc(traindata, trainlabel, validData, validLabel, testData):#, testdata):
     print("Start training SVM...\n" + SEPARATOR)
     svcClf = SVC(kernel="rbf", verbose=True, decision_function_shape='ovo', probability=True, cache_size=900)
     svcClf.fit(traindata, trainlabel)
 
     score=svcClf.score(validData, validLabel)
     print("Mean validation accuracy: "+str(score))
-   # pred_testlabel = svcClf.predict(testdata)
+    pred_test = svcClf.predict_proba(testData)
 
+    return pred_test
 
-def rf(traindata, trainlabel, validData, validLabel):
+def rf(traindata, trainlabel, validData, validLabel, testData):
     print("Start training Random Forest...\n" + SEPARATOR)
-    rfClf = RandomForestClassifier(n_estimators=1000, oob_score=True, n_jobs=4, random_state=RDM, verbose=20, criterion='gini')
+    rfClf = RandomForestClassifier(n_estimators=1000, oob_score=True, n_jobs=-1, random_state=RDM, verbose=20, criterion='gini')
     rfClf.fit(traindata, trainlabel)
 
     score =rfClf.score(validData, validLabel)
     print("Mean validation accuracy: "+str(score))
 
-    #pred_testlabel = rfClf.predict(testdata)
+    pred_test = rfClf.predict_proba(testData)
 
+    return pred_test
 
 if __name__ == '__main__':
 
-    if (loadFeatures):
-        print("Loading features...\n" + SEPARATOR)
-        feature_train = np.load('saved_data/feaExt_Train' + str(imgSize) + '.npy')
-        feature_valid = np.load('saved_data/feaExt_Valid' + str(imgSize) + '.npy')
-        train_target = np.load('saved_data/feaExt_Train' + str(imgSize) + '_target.npy')
-        valid_target = np.load('saved_data/feaExt_Valid' + str(imgSize) + '_target.npy')
-        feature_test = np.load('saved_data/fea_Test' + str(imgSize) + '.npy')
-        test_id = np.load('saved_data/test_id.npy')
+    if (loadFeatures is False):
+        create_feature_extractor()
 
-    else:
-        feature = create_feature_extractor()
+    print("Loading features...\n" + SEPARATOR)
+    feature_train = np.load('saved_data/feaExt_Train' + str(imgSize) + '.npy')
+    feature_valid = np.load('saved_data/feaExt_Valid' + str(imgSize) + '.npy')
+    train_target = np.load('saved_data/feaExt_Train' + str(imgSize) + '_target.npy')
+    valid_target = np.load('saved_data/feaExt_Valid' + str(imgSize) + '_target.npy')
+    feature_test = np.load('saved_data/fea_Test' + str(imgSize) + '.npy')
+    test_id = np.load('saved_data/test_id.npy')
 
-   # print(feature)
 
     # Turn feature maps into 2dim arrays
     print(feature_train.shape)
@@ -71,8 +71,16 @@ if __name__ == '__main__':
     d2_valid_features = scaler.fit_transform(d2_valid_features)
 
     if(clasif == "SVM"):
-        svc(d2_train_features, train_target, d2_valid_features, valid_target)
+        pred = svc(d2_train_features, train_target, d2_valid_features, valid_target)
     elif(clasif == "RF"):
-        rf (d2_train_features, train_target, d2_valid_features, valid_target)
+        pred = rf (d2_train_features, train_target, d2_valid_features, valid_target)
     else:
         print("Error, clasificador no instanciado")
+        exit(0)
+
+    currentDate = datetime.today()
+    timeStamp = currentDate.strftime("%d-%m-%Y_%H-%M")
+    df = pd.DataFrame(pred, columns=['Type_1', 'Type_2', 'Type_3'])
+    df['image_name'] = test_id
+
+    df.to_csv("../submission/" + clasif + "_" + timeStamp + ".csv", index=False)
--- Python-scripts/extract_features.py
+++ Python-scripts/extract_features.py
@@ -5,7 +5,8 @@ import numpy as np
 
 from keras import backend as K
 from sklearn.model_selection import train_test_split
-
+from keras.applications.inception_v3 import InceptionV3
+from keras.models import load_model, Model
 from image_utils import ImageUtils
 from data_augmentation import DataAugmentation as da
 
@@ -15,12 +16,13 @@ useAditional = True
 keepAspectRatio = True
 useKaggleData = False
 batchSize = 32
-percentTrainForValidation = 0.1
+percentTrainForValidation = 0.05
 useCustomPretrainedModels = True
 RDM = 17
-dataAugmentation = False
+dataAugmentation = True
 loadPreviousModel = True
-pathToPreviousModel = ""
+pathToPreviousModel = "saved_data/VGG16_fine-tunned_ep00_13-06-2017_17-26.hdf5"
+ftModel = "VGG16"  # IV3/VGG16/ = InceptionV3[Min.139|Def.299]/VGG16[Min.48|Def.224]
 SEPARATOR = "=============================================================" + \
             "==================="
 
@@ -68,10 +70,8 @@ def create_feature_extractor():
     print("\nLoading test data...\n" + SEPARATOR)
     if (keepAspectRatio):
         test_data = np.load('saved_data/test' + str(imgSize) + '_OrigAspectRatio.npy')
-        test_id = np.load('saved_data/test_id.npy')
     else:
         test_data = np.load('saved_data/test' + str(imgSize) + '.npy')
-        test_id = np.load('saved_data/test_id.npy')
 
 
     if(dataAugmentation):
@@ -80,24 +80,34 @@ def create_feature_extractor():
 
     print("\nCreating model...\n" + SEPARATOR)
     if (loadPreviousModel):
-        model = load_model(pathToPreviousModel)
+        baseModel = load_model(pathToPreviousModel)
         print("Loaded model from: " + pathToPreviousModel)
+
+        if (ftModel == "VGG16"):
+            model = Model(input=baseModel.input, outputs=baseModel.get_layer("block5_pool").output)
+        elif (ftModel =="IV3"):
+            model = Model(input=baseModel.input, outputs=baseModel.get_layer("mixed10").output)
     else:
-        model = create_pretrained_model(ftModel, ftApply)
-    # loading VGG16 model weights
-    model = VGG16(weights='imagenet', include_top=False, input_shape=(3, imgSize, imgSize))
+        if(ftModel == "VGG16"):
+            # loading VGG16 model weights
+            model = VGG16(weights='imagenet', include_top=False, input_shape=(3, imgSize, imgSize))
+        elif (ftModel =="IV3"):
+            model = InceptionV3(weights='imagenet', include_top=False, input_shape=(3, imgSize, imgSize))
 
     # Extracting features from the train dataset using the VGG16 pre-trained model
     print("\nGenerating features...\n" + SEPARATOR)
     if(dataAugmentation):
         #predict_generator(self, generator, steps, max_q_size=10, workers=1, pickle_safe=False, verbose=1)
         # TODO dar mas imagenes
-        features_train=model.predict_generator(datagen.flow(x_train, y_train, batch_size=batchSize, shuffle=True), len(x_train), verbose=1)
+        features_train=model.predict_generator(datagen.flow(x_train, y_train, batch_size=batchSize, shuffle=True), len(x_train)*2, verbose=1)
         features_valid = model.predict_generator(datagen.flow(x_val_train, y_val_train, batch_size=batchSize, shuffle=True),
                                                  len(x_val_train), verbose=1)
+        features_test = model.predict(test_data, batch_size=batchSize, verbose=1)
+
     else:
         features_train=model.predict(x_train, batch_size=batchSize, verbose=1)
-        features_valid=model.predict(x_val_train, batch_size=batchSize, verbose=1 )
+        features_valid=model.predict(x_val_train, batch_size=batchSize, verbose=1)
+        features_test = model.predict(test_data, batch_size=batchSize, verbose=1)
 
 
     if(dataAugmentation):
@@ -105,6 +115,13 @@ def create_feature_extractor():
                     allow_pickle=True, fix_imports=True)
         np.save('saved_data/feaExt_DAValid' + str(imgSize) + '.npy', features_valid,
                     allow_pickle=True, fix_imports=True)
+        np.save('saved_data/feaExt_DATrain' + str(imgSize) + '_target.npy', y_train,
+                allow_pickle=True, fix_imports=True)
+
+        np.save('saved_data/feaExt_DAValid' + str(imgSize) + '_target.npy', y_val_train,
+                allow_pickle=True, fix_imports=True)
+        np.save('saved_data/feaExt_test' + str(imgSize) + '.npy', features_test,
+                allow_pickle=True, fix_imports=True)
     else:
         np.save('saved_data/feaExt_Train' + str(imgSize) + '.npy', features_train,
                     allow_pickle=True, fix_imports=True)
@@ -113,6 +130,8 @@ def create_feature_extractor():
         np.save('saved_data/feaExt_Valid' + str(imgSize) + '.npy', features_valid,
                     allow_pickle=True, fix_imports=True)
         np.save('saved_data/feaExt_Valid' + str(imgSize) + '_target.npy', y_val_train,
+                allow_pickle=True, fix_imports=True)
+        np.save('saved_data/feaExt_test' + str(imgSize) + '.npy', features_test,
                 allow_pickle=True, fix_imports=True)
 
     return features_train
